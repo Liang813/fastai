4466119bb8ec790c479e3ae83c40aafd7dc8ba07
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-03-23 16:15:12 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from fastai.text import *
+print("TypeError: __init__() got an unexpected keyword argument 'auto_update'")
 # setup learner with IMDB data
 path = untar_data(URLs.IMDB_SAMPLE)
 data_clas = TextClasDataBunch.from_csv(path, 'texts.csv', bs=32)
@@ -11,7 +12,7 @@ probs, y_trues, reported_loss = learn.get_preds(with_loss=True, ordered=True)
 rng = 0  # either number or a range, e.g. range(0,3)
 calc_loss = F.cross_entropy(np.log(probs[rng]).reshape(-1,len(learn.data.classes)), # log(probs) reverses the softmax (ignoring the original scale)
                 y_trues[rng].reshape(-1), reduction='none')
-print("TypeError: __init__() got an unexpected keyword argument 'auto_update'")
+
 # check whether losses match
 assert np.all(np.array(calc_loss) - np.array(reported_loss[rng]) < 0.0001)
 
